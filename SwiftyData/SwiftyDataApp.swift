1c1a652bce9b99b50b1615ef5c881ef9f09eddad
Mert Durkaya <mertdurkaya@gmail.com>
2023-10-23 12:17:54 +0200

    Initial Commit

---
+++ SwiftyData/SwiftyDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyDataApp.swift
+//  SwiftyData
+//
+//  Created by Mert Durkaya on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
